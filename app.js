9a7b0f294ecd643f3e95069e50468e40e462875a
ogryaznov <gryaznovoleg2002@gmail.com>
2022-11-27 21:10:08 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//підключаємо express
+const express = require("express");
+//створюємо об'єкт додатка
+const app = express();
+
+//визначаємо обробник для маршруту "/"
+app.get("/", (request, response) => {
+	//відправляємо відповідь
+	response.send("Hello World!");
+});
+
+//починаємо прослуховувати підключення на 3000 порту
+app.listen(3000);
